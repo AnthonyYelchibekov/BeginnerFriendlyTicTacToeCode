32843390734bcc635dbc54ca2d66f15937890d5a
Anthony Yelchibekov <108062346+AnthonyYelchibekov@users.noreply.github.com>
2022-07-31 03:50:19 -0700

    File Dump
    HTML, CSS, JS and images uploaded related to tic tac toe game

---
+++ script.js
@@ -0,0 +1,129 @@
+let i=0
+
+        var one = document.getElementById("one");
+    one.onclick = markOne;
+
+    function markOne(){
+        if (i === 0) {
+            one.classList.add('x')
+            i++
+        } else {
+            i--
+            one.classList.add('o')
+        
+        }
+    }
+
+    var two = document.getElementById("two");
+    two.onclick = markTwo;
+
+    function markTwo(){
+        if (i === 0) {
+            two.classList.add('x')
+            i++
+        } else {
+            i--
+            two.classList.add('o')
+            
+        }
+    }
+
+    var three = document.getElementById("three");
+    three.onclick = markThree;
+
+    function markThree(){
+        if (i === 0) {
+            three.classList.add('x')
+            i++
+        } else {
+            i--
+            three.classList.add('o')
+            
+        }
+    }
+
+    
+    var four = document.getElementById("four");
+    four.onclick = markFour;
+
+    function markFour(){
+        if (i === 0) {
+            four.classList.add('x')
+            i++
+        } else {
+            i--
+            four.classList.add('o')
+            
+        }
+    }
+
+    var five = document.getElementById("five");
+    five.onclick = markFive;
+
+    function markFive(){
+        if (i === 0) {
+            five.classList.add('x')
+            i++
+        } else {
+            i--
+            five.classList.add('o')
+            
+        }
+    }
+
+    var six = document.getElementById("six");
+    six.onclick = markSix;
+
+    function markSix(){
+        if (i === 0) {
+            six.classList.add('x')
+            i++
+        } else {
+            i--
+            six.classList.add('o')
+            
+        }
+    }
+
+    var seven = document.getElementById("seven");
+    seven.onclick = markSeven;
+
+    function markSeven(){
+        if (i === 0) {
+            seven.classList.add('x')
+            i++
+        } else {
+            i--
+            seven.classList.add('o')
+            
+        }
+    }
+
+    var eight = document.getElementById("eight");
+    eight.onclick = markEight;
+
+    function markEight(){
+        if (i === 0) {
+            eight.classList.add('x')
+            i++
+        } else {
+            i--
+            eight.classList.add('o')
+            
+        }
+    }
+
+    
+    var nine = document.getElementById("nine");
+    nine.onclick = markNine;
+
+    function markNine(){
+        if (i === 0) {
+            nine.classList.add('x')
+            i++
+        } else {
+            i--
+            nine.classList.add('o')
+            
+        }
+    }
