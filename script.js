c6e20f6f9b1935ac2d6d4458c5ec48fa2a9fef62
Anthony Yelchibekov <108062346+AnthonyYelchibekov@users.noreply.github.com>
2022-07-31 18:31:47 -0700

    Fixed Equals Type
    Removed extra "==" causing issues with updating the array

--- script.js
+++ script.js
@@ -11,11 +11,11 @@ array = [0,0,0,
         if (i === 0) {
             one.classList.add('x')
             i++
-            array[0] === 1
+            array[0] = 1
         } else {
             i--
             one.classList.add('o')
-            array[0] === 2
+            array[0] = 2
         }
     }
 
@@ -26,11 +26,11 @@ array = [0,0,0,
         if (i === 0) {
             two.classList.add('x')
             i++
-            array[1] === 1
+            array[1] = 1
         } else {
             i--
             two.classList.add('o')
-            array[1] === 2
+            array[1] = 2
         }
     }
 
@@ -41,11 +41,11 @@ array = [0,0,0,
         if (i === 0) {
             three.classList.add('x')
             i++
-            array[2] === 1
+            array[2] = 1
         } else {
             i--
             three.classList.add('o')
-            array[2] === 2
+            array[2] = 2
         }
     }
 
@@ -57,11 +57,11 @@ array = [0,0,0,
         if (i === 0) {
             four.classList.add('x')
             i++
-            array[3] === 1
+            array[3] = 1
         } else {
             i--
             four.classList.add('o')
-            array[3] === 2
+            array[3] = 2
         }
     }
 
@@ -72,11 +72,11 @@ array = [0,0,0,
         if (i === 0) {
             five.classList.add('x')
             i++
-            array[4] === 1
+            array[4] = 1
         } else {
             i--
             five.classList.add('o')
-            array[4] === 2
+            array[4] = 2
         }
     }
 
@@ -87,11 +87,11 @@ array = [0,0,0,
         if (i === 0) {
             six.classList.add('x')
             i++
-            array[5] === 1
+            array[5] = 1
         } else {
             i--
             six.classList.add('o')
-            array[5] === 2
+            array[5] = 2
         }
     }
 
@@ -102,11 +102,11 @@ array = [0,0,0,
         if (i === 0) {
             seven.classList.add('x')
             i++
-            array[6] === 1
+            array[6] = 1
         } else {
             i--
             seven.classList.add('o')
-            array[6] === 2
+            array[6] = 2
         }
     }
 
@@ -117,11 +117,11 @@ array = [0,0,0,
         if (i === 0) {
             eight.classList.add('x')
             i++
-            array[7] === 1
+            array[7] = 1
         } else {
             i--
             eight.classList.add('o')
-            array[7] === 2
+            array[7] = 2
         }
     }
 
@@ -133,10 +133,18 @@ array = [0,0,0,
         if (i === 0) {
             nine.classList.add('x')
             i++
-            array[8] === 1
+            array[8] = 1
         } else {
             i--
             nine.classList.add('o')
-            array[8] === 2
+            array[8] = 2
         }
     }
+
+    function redWins() {
+        if(array == [1,1,1,2,2,0,0,0,0]){
+            alert('red wins!')
+        }
+    }
+    
+    redWins()
